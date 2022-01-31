127d04d5e3e1eacce618d81e92b2134742b39bf8
Sadat <saadatt@gmail.com>
2022-01-31 16:16:17 -0600

    updated testCases

--- src/_tests_/interview.test.js
+++ src/_tests_/interview.test.js
@@ -2,14 +2,12 @@ import {sum} from '../interview';
 
 describe('test result function', () => {
     const testCases = [
-        {value:[], expected: 0},
-        {value:[1], expected: 1},
-        {value: [5, 7, 10], expected: 22},
-        {value: [4, 3, 1, 6, 7, 8], expected: 29},
-        {value: [8, 8], expected: 16},
-        {value: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], expected: 55},
-    ]
-    it.each(testCases)('should return %#', ({value, expected}) => {
-        expect(sum(value)).toEqual(expected)
+        ['for empty array returns 0', [], 0],
+        ['for single-value array returns the value itself', [1], 1],
+        ['for multiple-value array returns its sum', [1, 2], 3],
+        ['for multiple-value array returns its sum', [5, 7, 10], 22],
+    ];
+    it.each(testCases)('case %#: %s', (_name, arr, exp) => {
+        expect(sum(arr)).toEqual(exp);
     });
-    });
+});
